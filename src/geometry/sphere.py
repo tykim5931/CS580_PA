c202c7eff1aeb615fbb0c41aecc082bfb032f419
tykim5931 <tykim5931@gmail.com>
2024-04-01 09:19:27 +0900

    feat: sphere intersection

--- src/geometry/sphere.py
+++ src/geometry/sphere.py
@@ -34,7 +34,42 @@ class Sphere_Collider(Collider):
           the ray distance to the intersection point (row 0)
           and the orientation of the intersection point (row 1).
         """
-        raise NotImplementedError("TODO")
+        # raise NotImplementedError("TODO")
+        
+        # R(t) = O +tD , (P-C)^2 - r^2 = 0
+        # t^2 (D⋅D)+2t(D⋅(O−C))+(O−C)⋅(O−C)−r^2 =0
+        # t = -b +- sqrt(discriminat) / 2a
+        a = D.dot(D)
+        O_C = O-self.center
+        b = 2*D.dot(O_C)
+        c = (O_C).dot(O_C) - self.radius**2
+        discriminant = b**2 - 4*a*c
+        
+        # if disc < 0, ray does not intersect
+        # if disc > 0, intersect with 2
+        # if disc = 0, ray meets sphere 1
+        # let disc > 0.
+        r1 = (-b - np.sqrt(np.abs(discriminant))) / (2*a)
+        r2 = (-b + np.sqrt(np.abs(discriminant))) / (2*a)
+        r= np.where((r1 > 0) & (r1 < r2), r1, r2)
+        M = O + D*r # intersection point
+        NdotD = ((M-self.center)/self.radius).dot(D)
+        
+        hit = (discriminant > 0) & (r>0)
+        hit_UPWARDS = NdotD < 0
+        hit_UPDOWN = np.logical_not(hit_UPWARDS)
+
+        pred1 = hit & hit_UPWARDS
+        pred2 = hit & hit_UPDOWN
+        pred3 = True
+        return np.select(
+            [pred1, pred2, pred3],
+            [
+                [r, np.tile(UPWARDS, r.shape)],
+                [r, np.tile(UPDOWN, r.shape)],
+                FARAWAY,
+            ],
+        )
 
     def get_Normal(self, hit):
         # M = intersection point
